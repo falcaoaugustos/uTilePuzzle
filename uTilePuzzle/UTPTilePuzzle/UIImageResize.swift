b9432bdad568e6afb8c00829167df6eee48dd3a4
Augusto Falcao <falcaoaugustos@gmail.com>
2017-09-22 02:21:40 -0300

    feat (image crop): implement UIImage extension

---
+++ uTilePuzzle/UTPTilePuzzle/UIImageResize.swift
@@ -0,0 +1,45 @@
+//
+//  UIImageResize.swift
+//  uTilePuzzle
+//
+//  Created by Augusto Falcão on 9/22/17.
+//  Copyright © 2017 Augusto Falcão. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+    func resizedImageWithSize(size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let sourceImage = self.cgImage?.copy()
+        var newImage = UIImage(cgImage: sourceImage!, scale: 0.0, orientation: self.imageOrientation)
+        newImage.draw(in: CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height))
+        newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
+    func kGetScale() -> CGFloat {
+        return 1.0
+    }
+
+    func cropImageFromFrame(frame: CGRect) -> UIImage {
+        let destFrame = CGRect(x: 0.0, y: 0.0, width: frame.size.width, height: frame.size.height)
+
+        var scale: CGFloat = 1.0
+
+        if UIScreen.main.responds(to: #selector(kGetScale)) {
+            scale = UIScreen.main.scale
+        }
+
+        let sourceFrame = CGRect(x: frame.origin.x * scale, y: frame.origin.y * scale, width: frame.size.width * scale, height: frame.size.height * scale)
+
+        UIGraphicsBeginImageContextWithOptions(destFrame.size, false, 0.0)
+        let sourceImage = self.cgImage?.cropping(to: sourceFrame)
+        var newImage = UIImage(cgImage: sourceImage!, scale: 0.0, orientation: self.imageOrientation)
+        newImage.draw(in: destFrame)
+        newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
